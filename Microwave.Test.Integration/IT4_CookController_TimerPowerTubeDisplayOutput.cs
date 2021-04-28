1eddd31c7d8f992ea734342fb310e074f1ee32bb
anekatdj <60734487+anekatdj@users.noreply.github.com>
2021-04-28 09:56:28 +0200

    Vi forsøgte at lave integrationstest til de sidste tre metoder i CookController

--- Microwave.Test.Integration/IT4_CookController_TimerPowerTubeDisplayOutput.cs
+++ Microwave.Test.Integration/IT4_CookController_TimerPowerTubeDisplayOutput.cs
@@ -2,6 +2,7 @@
 using Microwave.Classes.Boundary;
 using Microwave.Classes.Controllers;
 using Microwave.Classes.Interfaces;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace Microwave.Test.Integration
@@ -13,7 +14,7 @@ namespace Microwave.Test.Integration
         private IOutput _output;
         private IDisplay _display;
         private IPowerTube _powerTube;
-        private CookController _uut;
+        private CookController _sut;
 
         [SetUp]
         public void SetUp()
@@ -21,22 +22,22 @@ namespace Microwave.Test.Integration
             _timer = new Timer();
             _output = new Output();
             _display = new Display(_output);
-            _powerTube = new PowerTube(_output);
-            _uut = new CookController(_timer, _display, _powerTube);
+            _powerTube = new PowerTube(_output); //Substitute.For<IPowerTube>(); //
+            _sut = new CookController(_timer, _display, _powerTube);
         }
 
         [TestCase(101, 10)]
         [TestCase(0, 10)]
         public void StartCooking_PowerOutOfRange_ThrowsException(int power, int time)
         {
-            Assert.That(() => _uut.StartCooking(power, time), Throws.TypeOf<ArgumentOutOfRangeException>());
+            Assert.That(() => _sut.StartCooking(power, time), Throws.TypeOf<ArgumentOutOfRangeException>());
         }
 
 
         [TestCase(100, 101)]
         public void StartCooking_PowerInRange_ThrowsNothing(int power, int time)
         {
-            Assert.That(() => _uut.StartCooking(power, time), Throws.Nothing);
+            Assert.That(() => _sut.StartCooking(power, time), Throws.Nothing);
         }
 
         [TestCase(100, 101)]
@@ -44,8 +45,18 @@ namespace Microwave.Test.Integration
         [TestCase(1, 101)]
         public void StartCooking_PowerInRange_TimeRemainingIsEqualToTime(int power, int time)
         {
-            _uut.StartCooking(power, time);
+            _sut.StartCooking(power, time);
             Assert.That(_timer.TimeRemaining, Is.EqualTo(time));
         }
+
+        [Test]
+        public void Stop_()
+        {
+            _sut.Stop();
+            _powerTube.Received(1).TurnOff();
+        }
+
+        //[Test]
+
     }
 }
