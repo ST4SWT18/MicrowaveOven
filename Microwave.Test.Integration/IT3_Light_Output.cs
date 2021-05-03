fca2dcee7611b4ed4ebe06e25df7b27dabd7bc93
anekatdj <60734487+anekatdj@users.noreply.github.com>
2021-05-03 16:10:42 +0200

    Test rettet i Light, men failer ved TurnOff metoder

--- Microwave.Test.Integration/IT3_Light_Output.cs
+++ Microwave.Test.Integration/IT3_Light_Output.cs
@@ -1,4 +1,5 @@
-﻿using Microwave.Classes.Boundary;
+﻿using System.IO;
+using Microwave.Classes.Boundary;
 using Microwave.Classes.Interfaces;
 using NSubstitute;
 using NUnit.Framework;
@@ -10,26 +11,38 @@ namespace Microwave.Test.Integration
     {
         private IOutput _output;
         private ILight _sut;
+        private StringWriter _readConsole;
 
         [SetUp]
         public void SetUp()
         {
             _output = new Output();
             _sut = new Light(_output);
+            _readConsole = new StringWriter();
+            System.Console.SetOut(_readConsole);
         }
 
         [Test]
         public void TurnOn_LightIsTurnedOnIsWritten()
         {
             _sut.TurnOn();
-            _output.OutputLine(Arg.Is<string>(str => str.ToLower().Contains($"on")));
+
+            var text = _readConsole.ToString();
+
+            Assert.That(text, Is.EqualTo("Light is turned on\r\n"));
+
+            //_output.OutputLine(Arg.Is<string>(str => str.ToLower().Contains($"on")));
         }
 
         [Test]
         public void TurnOff_LightIsTurnedOffIsWritten()
         {
             _sut.TurnOff();
-            _output.OutputLine(Arg.Is<string>(str => str.ToLower().Contains("off")));
+
+            var text = _readConsole.ToString();
+
+            Assert.That(text, Is.EqualTo("Light is turned off\r\n"));
+            //_output.OutputLine(Arg.Is<string>(str => str.ToLower().Contains("off")));
         }
     }
 }
