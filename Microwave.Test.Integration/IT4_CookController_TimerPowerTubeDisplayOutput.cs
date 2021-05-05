b4119b512dab3dcd82fb47157805597706837438
laurabirchx <57993207+laurabirchx@users.noreply.github.com>
2021-05-05 09:04:03 +0200

    smårettelser

--- Microwave.Test.Integration/IT4_CookController_TimerPowerTubeDisplayOutput.cs
+++ Microwave.Test.Integration/IT4_CookController_TimerPowerTubeDisplayOutput.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.IO;
 using Microwave.Classes.Boundary;
 using Microwave.Classes.Controllers;
 using Microwave.Classes.Interfaces;
 using NSubstitute;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
 
 namespace Microwave.Test.Integration
 {
@@ -15,6 +17,7 @@ namespace Microwave.Test.Integration
         private IDisplay _display;
         private IPowerTube _powerTube;
         private ICookController _sut;
+        private StringWriter _readConsole;
 
         [SetUp]
         public void SetUp()
@@ -24,6 +27,8 @@ namespace Microwave.Test.Integration
             _display = new Display(_output);
             _powerTube = new PowerTube(_output); //Substitute.For<IPowerTube>(); //
             _sut = new CookController(_timer, _display, _powerTube);
+            _readConsole = new StringWriter();
+            System.Console.SetOut(_readConsole);
         }
 
         [TestCase(701, 10)]
@@ -34,21 +39,41 @@ namespace Microwave.Test.Integration
         }
 
 
-        [TestCase(100, 101)]
+        [TestCase(50, 101)]
+        [TestCase(700, 101)]
         public void StartCooking_PowerInRange_ThrowsNothing(int power, int time)
         {
             Assert.That(() => _sut.StartCooking(power, time), Throws.Nothing);
         }
 
-        [TestCase(100, 101)]
         [TestCase(50, 101)]
-        [TestCase(1, 101)]
+        [TestCase(700, 101)]
+        public void StartCooking_PowerInRange_PowerIsCorrectlyWritten(int power, int time)
+        {
+            _sut.StartCooking(power, time);
+            var text = _readConsole.ToString();
+            Assert.That(text, Is.EqualTo($"PowerTube works with {power}\r\n"));
+        }
+
+
+        [TestCase(700, 101)]
+        [TestCase(50, 101)]
         public void StartCooking_PowerInRange_TimeRemainingIsEqualToTime(int power, int time)
         {
             _sut.StartCooking(power, time);
             Assert.That(_timer.TimeRemaining, Is.EqualTo(time));
         }
 
-        
+
+        [TestCase(50,101)]
+        [TestCase(700,101)]
+        public void Stop_PowerTubeIsTurnedOff(int power, int time)
+        {
+            //_sut.StartCooking(power, time);
+            //_sut.Stop();
+
+            //var text = _readConsole.ToString();
+            //Assert.That(text, Is.EqualTo($"PowerTube works with 50\r\nPowerTube turned off\r\n"));
+        }
     }
 }
