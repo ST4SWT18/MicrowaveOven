d20b2529ffffcc6aae537fc9874c94c9304c3baa
laurabirchx <57993207+laurabirchx@users.noreply.github.com>
2021-05-05 11:35:35 +0200

    IT4

--- Microwave.Test.Integration/IT4_CookController_TimerPowerTubeDisplayOutput.cs
+++ Microwave.Test.Integration/IT4_CookController_TimerPowerTubeDisplayOutput.cs
@@ -36,6 +36,12 @@ namespace Microwave.Test.Integration
             _output = new Output();
             _display = new Display(_output);
 
+            _powerButton = new Button();
+            _timerButton = new Button();
+            _startCancelButton = new Button();
+            _door = new Door();
+            _light = new Light(_output);
+
             _powerTube = new PowerTube(_output);
             _sut = new CookController(_timer, _display, _powerTube);
             _userInterface = new UserInterface(_powerButton, _timerButton, _startCancelButton, _door, _display, _light, _sut);
@@ -90,17 +96,26 @@ namespace Microwave.Test.Integration
         }
         
 
-        [TestCase(50, 5)]
-        [TestCase(700, 5)]
+        [TestCase(50, 60)]
+        [TestCase(700, 60)]
         public void OnTimerTick_And_OnTimerExpired_ShowsTimeLeft_AndTimeDone(int power, int time)
         {
             string result = "";
-            _sut.StartCooking(power, time);
+            
+            _powerButton.Press();
+            result += string.Join("", $"Display shows: {power} W\r\n");
+
+            _timerButton.Press();
+            result += string.Join("", $"Display shows: 01:00\r\n");
+
+            _startCancelButton.Press();
+            result += string.Join("", $"Light is turned on\r\nPowerTube works with {power}\r\n");
+
             Thread.Sleep(6000);
 
             result += string.Join("", $"PowerTube works with {power}\r\n");
 
-            for (int i = 1; i < 6; i++)
+            for (int i = 1; i < 61; i++)
             {
                 result += string.Join("", $"Display shows: 00:0" + (time - i) + "\r\n");
             }
