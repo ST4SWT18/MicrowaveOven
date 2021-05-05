fab3c59928153b8c8cd41e0aae9d2353e1d6b8ba
anekatdj <60734487+anekatdj@users.noreply.github.com>
2021-05-05 11:07:57 +0200

    IT5 færdig

--- Microwave.Test.Integration/IT5_UserInterface_LightButtonDoorOutput.cs
+++ Microwave.Test.Integration/IT5_UserInterface_LightButtonDoorOutput.cs
@@ -42,7 +42,7 @@ namespace Microwave.Test.Integration
             _powerTube = new PowerTube(_output);
             _cookController = new CookController(_timer, _display, _powerTube);
             _sut = new UserInterface(_powerButton, _timerButton, _startCancelButton, _door, _display, _light, _cookController);
-            
+            _cookController.UI = _sut;
 
 
             _readConsole = new StringWriter();
@@ -264,31 +264,9 @@ namespace Microwave.Test.Integration
             Assert.That(text, Is.EqualTo(result));
         }
 
-        [Test]
-        public void CookingIsDone_StateCooking_CorrectTextIsDisplayed()
-        {
-            string result = "";
-
-            _powerButton.Press();
-            result += string.Join("", "Display shows: 50 W\r\n");
-
-            _timerButton.Press();
-            result += string.Join("", "Display shows: 01:00\r\n");
-
-            _startCancelButton.Press();
-            result += string.Join("", "Light is turned on\r\nPowerTube works with " + 50 + "\r\n");
-
-            _sut.CookingIsDone();
-            result += string.Join("", "Display cleared\r\nLight is turned off\r\n");
-
-            var text = _readConsole.ToString();
-
-            Assert.That(text, Is.EqualTo(result));
-        }
-
         //Test til at finde 1000 fejlen
         [Test]
-        public void Wait()
+        public void CookingIsDone_StateCooking_CorrectTextIsDisplayed()
         {
             string result = "";
 
@@ -313,7 +291,7 @@ namespace Microwave.Test.Integration
                 result += string.Join("", "Display shows: 00:0" + (60 - i) + "\r\n");
             }
 
-            result += string.Join("", "PowerTube turned off\r\n");
+            result += string.Join("", "PowerTube turned off\r\nDisplay cleared\r\nLight is turned off\r\n");
             var text = _readConsole.ToString();
 
             Assert.That(text, Is.EqualTo(result));
